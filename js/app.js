bf27f6590c70fc092ecfc30011319c1d6cfba7d8
Michael Abood <Schroeder@Mikes-Macbook.local>
2017-02-01 16:27:05 -0600

    updated files

---
+++ js/app.js
@@ -0,0 +1,23 @@
+console.log('page is loaded')
+
+var x = 2
+if (1 === x){
+console.log('1 === 2 yeahh')
+
+else {
+	console.log('1 does not equal x')
+};
+
+var foods = ["ramen", "soba", "crab rangoon", "tempura"];
+foods.shift(); 
+
+
+foods[1] = "soba"
+
+if (foods[0] === "ramen"){
+	console.log('yum ramen')
+}
+else if (foods[2] === "crab rangoon"){
+	console.log ('yummm ' + foods[2])
+}
+
