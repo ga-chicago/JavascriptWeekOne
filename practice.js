7814fc80f8fbdf22d2fb3a250fa7e320ebd28c2a
Naomi Mine <naomi.mine@gmail.com>
2017-02-01 19:34:20 -0600

    I did sum werk

---
+++ practice.js
@@ -0,0 +1,31 @@
+1.
+var pi = 3.14
+//Number (float)
+var name = "Naomi Charlotte Mine"
+//String
+var prime = [11, 17, 23, 53]
+//Object (array)
+var num = 87;
+//number
+var WDI = true
+//boolean
+
+
+2.
+var places = ["Prague", "England", "Spain"];
+places.shift();
+places.shift();
+places.unshift("Italy", "Brazil", "India");
+places.pop();
+places[1] = "Japan";
+places[0] = "Where am I going?";
+places.push("8");
+
+
+3.
+var age = 21;
+if (age >= 21) {
+	console.log("You may have a drink with me! ;)");
+} else {
+	console.log("Don't break the law, fool!");
+}
