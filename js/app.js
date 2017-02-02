c106c5f39b8a3925ed865f9b124758e42f467990
Michael Abood <Schroeder@Mikes-Macbook.local>
2017-02-02 00:56:22 -0600

    Completed day one homework

--- js/app.js
+++ js/app.js
@@ -1,23 +1,60 @@
-console.log('page is loaded')
+c//Part 1
+ 
+ 3.14 //Float
+ 
+ "Michael Abood" //String
+ 
+ [2, 3, 5, 7] //Object
+ 
+ 87 //Number
+ 
+ true != false //Boolean
+ 
+ //Part 2
+ 
+ var myTravels = ['California', 'Arizona', 'Florida'];
 
-var x = 2
-if (1 === x){
-console.log('1 === 2 yeahh')
-
-else {
-	console.log('1 does not equal x')
-};
-
-var foods = ["ramen", "soba", "crab rangoon", "tempura"];
-foods.shift(); 
-
-
-foods[1] = "soba"
-
-if (foods[0] === "ramen"){
-	console.log('yum ramen')
-}
-else if (foods[2] === "crab rangoon"){
-	console.log ('yummm ' + foods[2])
-}
-
+ myTravels.shift()
+ 
+ var myTravels = [ 0, 1];
+ 
+ 	myTravels.unshift()
+ 
+ var myTravels = ['Florida'];
+ 
+ 	myTravels.push('Portland', 'Austin', 'New York')
+ 
+ var myTravels = ["Florida", "Portland", "Austin", "New York"]
+ 
+ 	myTravels.pop()
+ 
+ var myTravels = ["Floria", "Portland", "Austin"]
+ 
+ 	myTravels[2] = "Japan"
+ 
+ var myTravels = ["Florida", "Portland", "Japan"]
+ 
+ 	myTravels[0] = "Where am I going?"
+ 
+ var myTravels = ["Where am I going?", "Portland", "Japan"]
+ 
+ 	myTravels.push(8)
+ 
+ var myTravels = ["Where am I going?", "Europe", "Japan", 8]
+ 
+ //Part 3
+ 
+ var personAge = 23;
+ 
+ if (personAge >= 21) {
+ 	console.log("You may drink the alcohols")
+ }
+ 	else if (personAge < 21) {
+ 		console.log("Don't even think about it")
+ 	}
+ 
+ 	else {
+ 		console.log("Not an age")
+ 	};
+ 
+ "you may drink the alcohols"
