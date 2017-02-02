c86fe322d3c617bbf9769799b8b7df4c3b95baeb
Nate <sauceynate@gmail.com>
2017-02-01 21:25:59 -0600

    updated with proper ;

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 var pi = (3.14);
 //number
-var myFullName = "Nathaniel Romero"
+var myFullName = "Nathaniel Romero";
 //string
-var primeNumbers = [1, 3, 5, 7, 11]
+var primeNumbers = [1, 3, 5, 7, 11];
 //object
-var num = 87
+var num = 87;
 //number
 var wdiOrNah = true;
 //boolean
 
-var places = ["california", "montana", "illinois", "spain", "new york", "france"]
+var places = ["california", "montana", "illinois", "spain", "new york", "france"];
 
-places.pop()
+places.pop();
 
-places[1] = "japan"
+places[1] = "japan";
 
-places[0] = "Where am I going"
+places[0] = "Where am I going";
 
-places.push(8)
+places.push(8);
 
-var myAge = (29)
+var myAge = (29);
 	if  (myAge >= 21){
 		console.log ('you may have a drink')
 	} else {
 		console.log ('you cannot break the law')
-	}
-	
+	};
