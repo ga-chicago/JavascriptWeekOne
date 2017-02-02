c094ce0b53fd4ab3b2a17ac4a60161cd40b3b128
ADEsposito <a.desposito640@gmail.com>
2017-02-01 20:58:53 -0600

    complete assignment; data types, array manipulation, conditional statement

---
+++ js/homework.js
@@ -0,0 +1,40 @@
+console.log("JavaScript is Working");
+
+// Data Types
+var pi = 3.14;     // number (float)
+
+var fullName = "Ann D'Esposito";     // string
+
+var primeNumbers = [2, 3, 5, 7, 11, 13];     // object
+
+var num = 87;     // number (int)
+
+var studentWDI = true;     // Boolean
+
+
+// Array Manipulation
+var travelArray = ["Iceland", "Ireland", "England"];
+
+travelArray.splice(0, 2);
+
+travelArray.push("Alaska", "Siberia", "Greenland");
+
+travelArray.pop();
+
+travelArray[2] = "Japan";
+
+travelArray[0] = "Where am I going?";
+
+travelArray.push(8);
+
+
+
+// Conditional Trials
+var age = 20;
+
+if (age >= 21) {
+  console.log("You may have a drink :)");
+}
+else {
+  console.log("Don't break the law!");
+}
